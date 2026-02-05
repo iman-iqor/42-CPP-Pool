9bde82123ee084f9d885b0a5d2b15b41d20fc15c
Iman Iqor <iqorimane111@gmail.com>
2026-02-05 22:06:34 +0100

    module08/ex02 sill in dev

---
+++ module08/ex02/MutantStack.hpp
@@ -0,0 +1,46 @@
+#ifndef MUTANTSTACK
+#define MUTANTSTACK
+
+#include <stack>
+#include <iterator>
+#include <iostream>
+#include <algorithm>
+
+/*
+std::stack is a container adapter.
+
+Internally, it uses another container (by default, std::deque) to store elements.
+
+It provides:
+
+push(), pop(), top(), size(), empty()
+
+But it does not provide iterators.
+
+The key trick here: std::stack stores elements in a member called c (protected).
+*/
+
+template<typename T>
+class MutantStack : public std::stack<T>
+{
+public:
+    MutantStack() : std::stack<T>() {}
+    MutantStack(const MutantStack& other) : std::stack<T>(other) {}
+    MutantStack& operator=(const MutantStack& other)
+    {
+        if (this != &other)
+            std::stack<T>::operator=(other);
+        return *this;
+    }
+    ~MutantStack() {}
+
+    typedef typename std::stack<T>::container_type::iterator iterator;
+    typedef typename std::stack<T>::container_type::const_iterator const_iterator;
+
+    iterator begin() { return this->c.begin(); }
+    iterator end() { return this->c.end(); }
+    const_iterator begin() const { return this->c.begin(); }
+    const_iterator end() const { return this->c.end(); }
+};
+
+#endif
