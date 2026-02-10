2b814d9771e46b58821a8a8693dcbdc3868f2903
Iman Iqor <iqorimane111@gmail.com>
2026-02-10 20:42:16 +0100

    module09/ex00 done

--- module09/ex00/BitcoinExchange.hpp
+++ module09/ex00/BitcoinExchange.hpp
@@ -7,7 +7,7 @@
 #include<fstream>
 #include<sstream>
 
-bool is_valid_file(int argc, char **argv);
+int is_valid_file(int argc, char **argv);
 std::string delete_spaces(std::string s);
 std::map<std::string, double> refDataMap(std::string fileName);
 bool isValidDate(const std::string &date);
