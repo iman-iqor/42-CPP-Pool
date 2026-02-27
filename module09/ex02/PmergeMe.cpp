026088a9840446e610749297c915bf244c2b3e0d
Iman Iqor <iqorimane111@gmail.com>
2026-02-27 22:45:41 +0000

    jacob algo done

--- module09/ex02/PmergeMe.cpp
+++ module09/ex02/PmergeMe.cpp
@@ -21,14 +21,42 @@ int binarySearchPosition(std::vector<int> &v, int value)
     }
     return left;
 }
+std::vector<size_t> generateJacobsthal(size_t pairsCount)
+{
+    std::vector<size_t> jacob;
 
+    if (pairsCount == 0)
+        return jacob;
+
+    size_t j0 = 0;
+    size_t j1 = 1;
+
+    if (j1 <= pairsCount)
+        jacob.push_back(j1);
+
+    while (1)
+    {
+        size_t next = j1 + 2 * j0;
+
+        if (next > pairsCount)
+            break;
+
+        if (next != j1)  // skip duplicate 1
+            jacob.push_back(next);
+
+        j0 = j1;
+        j1 = next;
+    }
+
+    return jacob;
+}
 int main(int argc, char **argv)
 {
     (void)argc;
     (void)argv;
     int array[] = {11, 2, 17, 0, 16, 8, 6, 15, 10, 3, 21, 1, 18, 9, 14, 19, 12, 5, 4, 20, 13, 7};
     std::vector<int> v(array, array + (sizeof(array) / sizeof(int)));
-    std::vector<std::pair<int, int>> paires;
+    std::vector<std::pair<int, int> > paires;
     int lenght = v.size();
     // std::cout<<lenght<<std::endl;
     if (lenght % 2 != 0)
@@ -75,25 +103,48 @@ int main(int argc, char **argv)
     {
         mainChain.push_back(paires[i].second);
     }
+    mainChain.insert(mainChain.begin(), paires[0].first);
+   
+
     std::vector<int> smallElements;
-    for (size_t i = 0; i < paires.size(); i++)
+    for (size_t i = 1; i < paires.size(); i++)
     {
         smallElements.push_back(paires[i].first);
     }
-    mainChain.insert(mainChain.begin(), smallElements[0]);
-    
-    for (size_t i = 1; i < paires.size(); i++)
+ 
+
+    std::vector<size_t> jacob = generateJacobsthal(smallElements.size());
+
+
+    std::vector<size_t> order;
+    size_t prev = 0;
+
+    for (size_t i = 0; i < jacob.size(); i++)
     {
-        int index = binarySearchPosition(mainChain, paires[i].first);
-        mainChain.insert(mainChain.begin() + index, paires[i].first);
+        size_t current = jacob[i];
+
+        for (size_t j = current; j > prev; j--)
+            order.push_back(j - 1); // convert to 0-based
+
+        prev = current;
+    }
+    
+    // remaining elements
+    for (size_t j = smallElements.size(); j > prev; j--)
+        order.push_back(j - 1);
+
+
+
+    for (size_t i = 0; i < order.size(); i++)
+    {
+        size_t index = order[i];
+
+        int value = smallElements[index];
+
+        int pos = binarySearchPosition(mainChain, value);
+
+        mainChain.insert(mainChain.begin() + pos, value);
     }
 
-    for (size_t i = 0; i < mainChain.size(); i++)
-    {
-        std::cout << mainChain[i] << std::endl;
-    }
-    for (size_t i = 0; i < paires.size(); i++)
-    {
-        std::cout << paires[i].first << " " << paires[i].second << std::endl;
-    }
+
 }
