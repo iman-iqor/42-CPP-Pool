3571c3686567284ffba1ae42b48c21429bd05d68
Iman Iqor <iqorimane111@gmail.com>
2026-02-28 17:06:42 +0000

    module09.ex02 spliting the code

--- module09/ex02/PmergeMe.cpp
+++ module09/ex02/PmergeMe.cpp
@@ -50,30 +50,20 @@ std::vector<size_t> generateJacobsthal(size_t pairsCount)
 
     return jacob;
 }
-int main(int argc, char **argv)
+std::vector<std::pair<int,int> > createPair(std::vector<int> &v)
 {
-    (void)argc;
-    (void)argv;
-    int array[] = {11, 2, 17, 0, 16, 8, 6, 15, 10, 3, 21, 1, 18, 9, 14, 19, 12, 5, 4, 20, 13, 7};
-    std::vector<int> v(array, array + (sizeof(array) / sizeof(int)));
-    std::vector<std::pair<int, int> > paires;
-    int lenght = v.size();
-    // std::cout<<lenght<<std::endl;
-    if (lenght % 2 != 0)
-    {
-        int last = v[lenght - 1];
-        (void)last;
-        v.pop_back();
-    }
-    // std::cout<<v.size()<<std::endl;
-
     std::pair<int, int> tmp;
+    std::vector<std::pair<int, int> > paires;
     for (size_t i = 0; i < v.size(); i++)
     {
         tmp.first = v[i];
         tmp.second = v[++i];
         paires.push_back(tmp);
     }
+    return paires;
+}
+std::vector<std::pair<int,int> > sortInsidePairs(std::vector<std::pair<int,int> > &paires)
+{
     for (size_t i = 0; i < paires.size(); i++)
     {
         if (paires[i].first > paires[i].second)
@@ -83,8 +73,11 @@ int main(int argc, char **argv)
             paires[i].second = tmp;
         }
     }
-    // sort the pairs
-    size_t i = 0;
+    return paires;
+}
+std::vector<std::pair<int ,int> > sortPaires(std::vector<std::pair<int,int> > &paires)
+{
+     size_t i = 0;
     while (i < paires.size())
     {
         size_t j = 0;
@@ -98,12 +91,47 @@ int main(int argc, char **argv)
         }
         i++;
     }
+    return paires;
+}
+std::vector<int> createMainChain(std::vector<std::pair<int,int> > paires)
+{
     std::vector<int> mainChain;
     for (size_t i = 0; i < paires.size(); i++)
     {
         mainChain.push_back(paires[i].second);
     }
     mainChain.insert(mainChain.begin(), paires[0].first);
+    return mainChain;
+}
+std::vector<int> createSmallElements(std::vector<std::pair<int,int> > paires)
+{
+    
+}
+int main(int argc, char **argv)
+{
+    (void)argc;
+    (void)argv;
+    int array[] = {11, 2, 17, 0, 16, 8, 6, 15, 10, 3, 21, 1, 18, 9, 14, 19, 12, 5, 4, 20, 13, 7,0};
+    std::vector<int> v(array, array + (sizeof(array) / sizeof(int)));
+
+    int lenght = v.size();
+    bool odd = false;
+    int last;
+    if (lenght % 2 != 0)
+    {
+        last = v[lenght - 1];
+        (void)last;
+        v.pop_back();
+        odd = true;
+    }
+
+
+    std::vector<std::pair<int, int> > paires=createPair(v);
+    paires = sortInsidePairs(paires);
+    paires = sortPaires(paires);
+   
+    std::vector<int> mainChain = createMainChain(paires);
+    
    
 
     std::vector<int> smallElements;
@@ -145,6 +173,18 @@ int main(int argc, char **argv)
 
         mainChain.insert(mainChain.begin() + pos, value);
     }
+    // 
+    
 
+   
+    if(odd)
+    {
+        int index = binarySearchPosition(mainChain,last);
+        mainChain.insert(mainChain.begin() + index,last);
+    }
+     for(size_t i = 0;i < mainChain.size();i++)
+    {
+        std::cout<<mainChain[i]<<std::endl;
+    }
 
 }
